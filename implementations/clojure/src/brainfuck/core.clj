d8c788b928e9e81baa754b166a7e0425e87cd2d6
Oliver Caldwell <olliec87@gmail.com>
2015-01-08 23:31:10 +0000

    Bulk of Clojure VM done. STDIN is wonky though.
    Can run a "Hello, World!"!

--- implementations/clojure/src/brainfuck/core.clj
+++ implementations/clojure/src/brainfuck/core.clj
@@ -54,9 +54,40 @@
       tokenise
       match-braces))
 
+(defn read-character [_]
+  "Reads a character from STDIN"
+  (.read System/in))
+
+(defn safe-inc [n]
+  "Treat nil values as zero."
+  (inc (if (= n nil) 0 n)))
+
+(defn safe-dec [n]
+  "Treat nil values as zero."
+  (dec (if (= n nil) 0 n)))
+
+(defmulti step (fn [program state] (:token (nth program (:program-pointer state)))))
+(defmethod step \> [program state] (update-in state [:memory-pointer] inc))
+(defmethod step \< [program state] (update-in state [:memory-pointer] dec))
+(defmethod step \+ [program state] (update-in state [:memory (:memory-pointer state)] safe-inc))
+(defmethod step \- [program state] (update-in state [:memory (:memory-pointer state)] safe-dec))
+(defmethod step \. [program state] (print (char (get-in state [:memory (:memory-pointer state)]))) state)
+(defmethod step \, [program state] (update-in state [:memory (:memory-pointer state)] read-character))
+(defmethod step \[ [program state] (if (= 0 (get-in state [:memory (:memory-pointer state)]))
+                                     (update-in state [:program-pointer] #(:destination (nth program %)))
+                                     state))
+(defmethod step \] [program state] (if (not (= 0 (get-in state [:memory (:memory-pointer state)])))
+                                     (update-in state [:program-pointer] #(:destination (nth program %)))
+                                     state))
+
 (defn execute [program]
   "Executes a compiled brainfuck program."
-  (println (apply str program)))
+  (let [program-length (count program)]
+    (loop [state {:memory (hash-map 0 0)
+                  :memory-pointer 0
+                  :program-pointer 0}]
+      (if (< (:program-pointer state) program-length)
+        (recur (update-in (step program state) [:program-pointer] inc))))))
 
 (defn -main
   "Entry point into the brainfuck interpreter. Takes a source file path to execute."
