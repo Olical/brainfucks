67bf7f7bcd7406f348d3253e8252042394d26ef3
Oliver Caldwell <olliec87@gmail.com>
2017-06-26 13:18:01 +0100

    Made rust read a file
    I didn't realise it used Options! :O nice!

--- implementations/rust/src/main.rs
+++ implementations/rust/src/main.rs
@@ -1,7 +1,32 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    if let Some(arg1) = env::args().nth(1) {
-        println!("The first argument is {}", arg1);
+    if let Some(path) = env::args().nth(1) {
+        let mut contents = String::new();
+        match File::open(path) {
+            Ok(mut file) => {
+                match file.read_to_string(&mut contents) {
+                    Ok(_) => execute(contents),
+                    Err(_) => println!("Found the file but failed to read it."),
+                }
+            }
+            Err(_) => println!("Failed to open the file for reading."),
+        };
+    } else {
+        println!("Please provide a path to some brainfuck source as the first argument.")
+    }
+}
+
+fn execute(source: String) {
+    println!("Executing: {}", source)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic() {
+        assert!(1 == 1);
     }
 }
