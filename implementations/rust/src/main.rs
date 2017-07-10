691fa587b5c669e2fc8134333c47429de094a997
Oliver Caldwell <olliec87@gmail.com>
2017-07-10 21:55:08 +0100

    Most of the typing donw with lots of tests
    Need to write the reader though.

--- implementations/rust/src/main.rs
+++ implementations/rust/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
         match File::open(path) {
             Ok(mut file) => {
                 match file.read_to_string(&mut contents) {
-                    Ok(_) => execute(contents),
+                    Ok(_) => println!("Would read and eval here."),
                     Err(_) => println!("Found the file but failed to read it."),
                 }
             }
@@ -19,14 +19,112 @@ fn main() {
     }
 }
 
-fn execute(source: String) {
-    println!("Executing: {}", source)
+mod brainfuck {
+    pub type Pointer = u32;
+
+    #[derive(PartialEq)]
+    #[derive(Debug)]
+    pub enum Command {
+        IncrementPointer,
+        DecrementPointer,
+        IncrementValue,
+        DecrementValue,
+        OutputValue,
+        InputValue,
+        ForwardsTo(Pointer),
+        BackwardsTo(Pointer)
+    }
+
+    #[derive(PartialEq)]
+    #[derive(Debug)]
+    pub enum ReadResult {
+        Program(Vec<Command>),
+        UnmatchedJump
+    }
+
+    pub fn read(source: &str) -> ReadResult {
+        let mut program: Vec<Command> = vec![];
+
+        // find braces first
+
+        return ReadResult::Program(program)
+    }
+
+    // pub fn eval(program: Vec<Command>) {
+    // }
 }
 
 #[cfg(test)]
 mod tests {
+    use brainfuck::*;
+    use brainfuck::Command::*;
+    use brainfuck::ReadResult::*;
+
     #[test]
-    fn basic() {
-        assert!(1 == 1);
+    fn read_empty() {
+        let source = "";
+        let expected = Program(vec![]);
+        let actual = read(source);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn read_simple() {
+        let source = "+>-<.,<.-+>,";
+        let expected = Program(vec![
+            IncrementValue,
+            IncrementPointer,
+            DecrementValue,
+            DecrementPointer,
+            OutputValue,
+            InputValue,
+            DecrementPointer,
+            OutputValue,
+            DecrementValue,
+            IncrementValue,
+            IncrementPointer,
+            InputValue
+        ]);
+        let actual = read(source);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn read_garbage() {
+        let source = ",.+lol,hey.>there<";
+        let expected = Program(vec![
+            InputValue,
+            OutputValue,
+            IncrementValue,
+            InputValue,
+            OutputValue,
+            IncrementPointer,
+            DecrementPointer
+        ]);
+        let actual = read(source);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn read_loop() {
+        let source = "[->+<]";
+        let expected = Program(vec![
+            ForwardsTo(5),
+            DecrementValue,
+            IncrementPointer,
+            IncrementValue,
+            DecrementPointer,
+            BackwardsTo(0)
+        ]);
+        let actual = read(source);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn read_bad_loop() {
+        let source = "[,.[,.>+-][./.[]]";
+        let expected = UnmatchedJump;
+        let actual = read(source);
+        assert_eq!(expected, actual);
     }
 }
